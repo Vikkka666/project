03281881af1f5c63ba28eb2a62b753fdf91e278e
Vika <taranskaavika@gmail.com>
2025-01-26 22:20:29 +0500

    Обновлен

--- src/app/components/main.js
+++ src/app/components/main.js
@@ -4,7 +4,11 @@ import React, { useEffect, useState } from 'react';
 export default function Main() {
   const [posts, setPosts] = useState([]); 
 
+  const [count, setCount] = useState(0);
+
   useEffect(() => {
+    document.title = `You clicked ${count} times`;
+
     const fetchData = async () => {
       try {
         const response = await fetch('http://flowershop/api/products');
@@ -22,7 +26,17 @@ export default function Main() {
   }, []); 
 
   return (
+    
     <div className='flex justify-around w-full flex-wrap'>
+      <div>
+      <button onClick={() => setCount(count - 1)}>
+        -1
+      </button>
+      <p>Количество кликов: {count} </p>
+      <button onClick={() => setCount(count + 1)}>
+        +1
+      </button>
+</div>
       {posts.map((post) => (
         <div key={post.id} className='w-[200px] m-[50px] bg-[#f2f2f2] p-[20px] rounded-lg'>
           <h2 className='text-lg font-bold mb-[10px]'> {post.name}</h2> 
